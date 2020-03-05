bb32afc39babf4574f9150cb14a5f4342d73379c
nambread <nambread@users.noreply.github.com>
2020-03-05 23:37:08 +0000

    Add python project
    Need a python project test ground

---
+++ EverythingPython-Tests/EverythingPython_Tests.py
@@ -0,0 +1,8 @@
+
+import os
+import sys
+
+pyd_path_debug = r'Everything-Python\build\x64\Debug'
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', pyd_path_debug))
+
+import Everything_Python
