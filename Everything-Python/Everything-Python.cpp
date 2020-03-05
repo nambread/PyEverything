7b7fdb3f429ae0e05fc8a8bb17090baf84abc229
nambread <nambread@users.noreply.github.com>
2020-03-05 23:36:14 +0000

    Test function
    Super simple search function test

--- Everything-Python/Everything-Python.cpp
+++ Everything-Python/Everything-Python.cpp
@@ -2,15 +2,39 @@
 //
 
 #include <iostream>
+#include <locale>
+#include <codecvt>
+
 #include "Everything.h"
 #include "pybind11/pybind11.h"
 
-int main()
+namespace py = pybind11;
+
+std::string ws2s(const std::wstring& wstr)
 {
-	const wchar_t* cheese = L"Cheese";
-	Everything_SetSearch(cheese);
+	using convert_typeX = std::codecvt_utf8<wchar_t>;
+	std::wstring_convert<convert_typeX, wchar_t> converterX;
+
+	return converterX.to_bytes(wstr);
 }
 
-PYBIND11_MODULE(ex, m) {
-	m.doc() = "Testing";
+std::string Py_Everything_Search(std::string searchString) {
+	const wchar_t* searchTerm = std::wstring(searchString.begin(), searchString.end()).c_str();
+	Everything_SetSearch(searchTerm);
+	bool ok = Everything_Query(true);
+	if (ok && Everything_GetNumResults() > 0) {
+		Everything_GetResultPath(0);
+		TCHAR buf[MAX_PATH];
+		Everything_GetResultFullPathName(0, buf, sizeof(buf) / sizeof(TCHAR));
+		const std::wstring result = std::wstring(buf);
+		return ws2s(result);
+	}
+	else {
+		return std::string("");
+	}
+}
+
+PYBIND11_MODULE(EverythingPython, m) {
+	m.doc() = "Everything Python Bindings.";
+	m.def("TestSearch", &Py_Everything_Search, py::arg("searchTerm"));
 }
