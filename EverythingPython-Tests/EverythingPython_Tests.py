3dc10aa7cf1d02dfd8e0fd88c30f60eff670d1fb
nambread <nambread@users.noreply.github.com>
2020-03-07 12:53:13 +0000

    Updated python test case

--- EverythingPython-Tests/EverythingPython_Tests.py
+++ EverythingPython-Tests/EverythingPython_Tests.py
@@ -3,6 +3,13 @@ import os
 import sys
 
 pyd_path_debug = r'Everything-Python\build\x64\Debug'
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', pyd_path_debug))
+pyd_path_resolved = os.path.join(os.path.dirname(__file__), '..', pyd_path_debug)
+sys.path.append(pyd_path_resolved)
+
+print(f"Added to path {pyd_path_resolved}")
 
 import Everything_Python
+
+testSearch = Everything_Python.TestSearch("system32")
+print(testSearch)
+'''break'''
