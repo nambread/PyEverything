b536cbd8d587930ef14f58793fa024ae7ed8eb41
nambread <nambread@users.noreply.github.com>
2020-03-07 22:46:40 +0000

    proof of concept script for searching
    Proof of concept usage for searching the hard drive.

--- EverythingPython-Tests/EverythingPython_Tests.py
+++ EverythingPython-Tests/EverythingPython_Tests.py
@@ -2,14 +2,35 @@
 import os
 import sys
 
-pyd_path_debug = r'Everything-Python\build\x64\Debug'
-pyd_path_resolved = os.path.join(os.path.dirname(__file__), '..', pyd_path_debug)
+project_root = os.path.normpath(os.path.join(os.path.dirname(__file__), '..'))
+
+pyd_path = r'Everything-Python\build\x64\Debug'
+pyd_path_resolved = os.path.join(project_root, pyd_path)
+dll_path = r'..\venv\Scripts'
+dll_path_resolved = os.path.join(project_root, dll_path)
 sys.path.append(pyd_path_resolved)
+sys.path.append(dll_path_resolved)
 
 print(f"Added to path {pyd_path_resolved}")
 
 import Everything_Python
+from Everything_Python import Query, SetSearch, GetNumResults, GetResultFullPathName
 
-testSearch = Everything_Python.TestSearch("system32")
-print(testSearch)
-'''break'''
+MAX_RESULTS = 30
+
+if __name__ == "__main__":
+    while True:
+        try:
+            search = input("Enter your search term: ")
+            if not search:
+                search = "\\"
+            SetSearch(search)
+            Query(wait=True)
+            numResults = GetNumResults()
+            maxResults = numResults if numResults < MAX_RESULTS else MAX_RESULTS
+            results = [GetResultFullPathName(i, 260) for i in range(maxResults)]
+            print("Printing the first 30 results:")
+            results = '\n'.join(results)
+            print(f"{results}")
+        except BaseException as e:
+            print(e)
