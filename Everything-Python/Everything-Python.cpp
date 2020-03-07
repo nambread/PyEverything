5b6dee4ef777e02412ed79f023e21b85028a87c7
nambread <nambread@users.noreply.github.com>
2020-03-07 14:03:47 +0000

    Get rid of conversions
    We don't need 'em, pybind handles wstrings perfectly fine.

--- Everything-Python/Everything-Python.cpp
+++ Everything-Python/Everything-Python.cpp
@@ -10,38 +10,9 @@
 
 namespace py = pybind11;
 
-std::string ws2s(const std::wstring& wstr)
-{
-	using convert_typeX = std::codecvt_utf8<wchar_t>;
-	std::wstring_convert<convert_typeX, wchar_t> converterX;
-
-	return converterX.to_bytes(wstr);
-}
-
-inline std::wstring convert(const std::string& as)
-{
-	// deal with trivial case of empty string
-	if (as.empty())    return std::wstring();
-
-	// determine required length of new string
-	size_t reqLength = ::MultiByteToWideChar(CP_UTF8, 0, as.c_str(), (int)as.length(), 0, 0);
-
-	// construct new string of required length
-	std::wstring ret(reqLength, L'\0');
-
-	// convert old string to new string
-	::MultiByteToWideChar(CP_UTF8, 0, as.c_str(), (int)as.length(), &ret[0], (int)ret.length());
-
-	// return new string ( compiler should optimize this away )
-	return ret;
-}
-
-std::wstring Py_Everything_Search(std::string searchString) {
-	size_t searchSize = searchString.size() * 2 + 2;
-	wchar_t* searchTerm = new wchar_t[searchSize];
-	swprintf(searchTerm, searchSize, L"%S", searchString.c_str());
-	Everything_SetSearch(searchTerm);
-	bool ok = Everything_Query(true);
+std::wstring Py_Test_Everything_Search(const std::wstring& searchString) {
+	Everything_SetSearch(searchString.c_str());
+	int ok = Everything_Query(true);
 	if (ok && Everything_GetNumResults() > 0) {
 		Everything_GetResultPath(0);
 		TCHAR buf[MAX_PATH];
@@ -54,7 +25,16 @@ std::wstring Py_Everything_Search(std::string searchString) {
 	}
 }
 
+void Py_Everything_Cleanup() {
+	Everything_CleanUp();
+}
+
+UINT32 Py_Everything_GetLastError() {
+	return Everything_GetLastError();
+}
+
+
 PYBIND11_MODULE(Everything_Python, m) {
 	m.doc() = "Everything Python Bindings.";
-	m.def("TestSearch", &Py_Everything_Search, py::arg("searchTerm"));
+	m.def("TestSearch", &Py_Test_Everything_Search, py::arg("searchTerm"));
 }
