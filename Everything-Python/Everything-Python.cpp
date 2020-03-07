a9210deac2587edad2feba8eeff8f2ad029a218c
nambread <nambread@users.noreply.github.com>
2020-03-07 22:46:03 +0000

    Bare minimum search functionality
    Bare minimum implemented to enable python-side searching

--- Everything-Python/Everything-Python.cpp
+++ Everything-Python/Everything-Python.cpp
@@ -10,19 +10,33 @@
 
 namespace py = pybind11;
 
+/*
+* Base exception for the everything API
+* Future plan is to break out each error code into its own exception so that python API users can catch specific errors that they care about
+* (e.g. catching the invalid ipc error to then boot the everything service.)
+*/
 struct EverythingException : std::exception {
 
-	const DWORD m_errorCode;
-	EverythingException(const DWORD& errorCode) : m_errorCode(errorCode) {}
+	const UINT32 m_errorCode;
+	EverythingException(const UINT32& errorCode) : m_errorCode(errorCode) {}
 
 	std::string errCodeString = std::to_string(this->m_errorCode);
 	const std::string m_errorMessage = "Everything API returned error code: " + errCodeString;
 	const char* what() const noexcept { return m_errorMessage.c_str(); }
 };
 
-static inline void checkEverythingErrorCodeAndThrow(const int& errorCode) {
-	if (!errorCode) {
-		throw EverythingException(Everything_GetLastError());
+/*
+Converts the error codes into exceptions that get thrown.
+@param status The status returned from various api calls. Most will return a 0 if the function fails.
+Gets a bit confusing in cases where the return value is also used as a count, where 0 is a valid value.
+*/
+static inline void checkEverythingErrorCodeAndThrow(const int& status) {
+	if (status == 0) {
+		UINT32 errorCode = Everything_GetLastError();
+		if (errorCode) {
+			//sometimes 0 is a valid status e.g when returning number of result matches.
+			throw EverythingException(errorCode);
+		}
 	}
 }
 
@@ -50,8 +64,40 @@ UINT32 Py_Everything_GetLastError() {
 	return Everything_GetLastError();
 }
 
+UINT32 Py_Everything_GetTotResults() {
+	UINT32 num = Everything_GetTotResults();
+	checkEverythingErrorCodeAndThrow(num);
+	return num;
+}
+
+UINT32 Py_Everything_GetNumResults() {
+	UINT32 num = Everything_GetNumResults();
+	checkEverythingErrorCodeAndThrow(num);
+	return num;
+}
+
+std::wstring Py_Everything_GetResultFullPathName(UINT32 index, const UINT32 maxCount = MAX_PATH) {
+	std::vector<wchar_t> buf = std::vector<wchar_t>(maxCount);
+	int ok = Everything_GetResultFullPathName(index, &buf[0], maxCount);
+	checkEverythingErrorCodeAndThrow(ok);
+	const std::wstring result = std::wstring(&buf[0]);
+	return result;
+}
+
+void Py_Everything_SetSearch(const std::wstring& searchTerm) {
+	Everything_SetSearch(searchTerm.c_str());
+}
+
+void Py_Everything_Query(bool wait = true) {
+	int ok = Everything_Query(wait);
+	checkEverythingErrorCodeAndThrow(ok);
+}
 
 PYBIND11_MODULE(Everything_Python, m) {
 	m.doc() = "Everything Python Bindings.";
-	m.def("TestSearch", &Py_Test_Everything_Search, py::arg("searchTerm"));
+	m.def("GetLastError", &Py_Everything_GetLastError);
+	m.def("GetNumResults", &Py_Everything_GetNumResults);
+	m.def("GetResultFullPathName", &Py_Everything_GetResultFullPathName, py::arg("index"), py::arg("max_count"));
+	m.def("SetSearch", &Py_Everything_SetSearch, py::arg("search_term"));
+	m.def("Query", &Py_Everything_Query, py::arg("wait"));
 }
