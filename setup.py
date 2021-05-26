937545268a1e32d0fd7b3e423268358584fbc3f2
nambread <nambread@users.noreply.github.com>
2021-05-27 00:00:35 +0100

    Move extension into the python package to include dll. Update paths in sln and project files.

--- setup.py
+++ setup.py
@@ -1,25 +1,32 @@
 from setuptools import setup, Extension, find_packages
+import os
+import shutil
 
 desc = '''
     
     '''
 
 pyd_ext = Extension('Everything_Python._Everything_Python', 
-                    ['_Everything_Python/Everything-Python.cpp'],
-                    include_dirs=['pybind11/include', 'EverythingSDK/include'],
+                    ['Everything_Python/_Everything_Python/Everything-Python.cpp'],
+                    include_dirs=['pybind11/include', 'Everything_Python/_Everything_Python/vendor/Everything-SDK/include'],
                     define_macros=[('_CRT_SECURE_NO_WARNINGS', None),('UNICODE', None)],
-                    library_dirs=['EverythingSDK/lib'],
+                    library_dirs=['Everything_Python/_Everything_Python/vendor/Everything-SDK/lib'],
                     libraries=['Everything64'])
 
-setup(
-        name='Everything_Python',
-        description='Search Everything SDK, but in python!',
-        version = '0.1',
-        author='Michael Nam',
-        author_email='michael.nam@namination.co.uk',
-        license='MIT',
-        url='',
-        package_data={'Everything_Python': ['./Everything64.dll']},
-        packages=['Everything_Python'],
-        ext_modules = [pyd_ext]
-    )
+#copy the dll to the pyd location, due to changes to how .pyd dependencies are loaded: https://docs.python.org/3/whatsnew/3.8.html#bpo-36085-whatsnew
+try:
+    shutil.copyfile('Everything_Python/_Everything_Python/vendor/Everything-SDK/dll/Everything64.dll', 'Everything_Python/Everything64.dll')
+    setup(
+            name='Everything_Python',
+            description='Search Everything SDK, but in python!',
+            version = '0.1',
+            author='Michael Nam',
+            author_email='michael.nam@namination.co.uk',
+            license='MIT',
+            url='',
+            package_data={'Everything_Python' : ['Everything64.dll']},
+            packages=['Everything_Python'],
+            ext_modules = [pyd_ext]
+        )
+finally:
+    os.remove('Everything_Python/Everything64.dll')
